a6c16d26d95a58e81422a6176ba1dfcccb393642
fg-uulm <florian.geiselhart@uni-ulm.de>
2020-03-24 19:22:37 +0100

    Fix week bug, add force append

--- fix_timetable.jsx
+++ fix_timetable.jsx
@@ -126,9 +126,10 @@ if (![].includes) {
  */
 
 
+
 /*
 Code for Import https://scriptui.joonas.me — (Triple click to select): 
-{"activeId":10,"items":{"item-0":{"id":0,"type":"Dialog","parentId":false,"style":{"enabled":true,"varName":null,"windowType":"Dialog","creationProps":{"su1PanelCoordinates":false,"maximizeButton":false,"minimizeButton":false,"independent":false,"closeButton":true,"borderless":false,"resizeable":false},"text":"Wochen auffüllen","preferredSize":[0,0],"margins":16,"orientation":"column","spacing":10,"alignChildren":["center","top"]}},"item-1":{"id":1,"type":"Checkbox","parentId":7,"style":{"enabled":true,"varName":"labweek","text":"Seminar- / Laborwoche berücksichtigen","preferredSize":[0,0],"alignment":"left","helpTip":null}},"item-2":{"id":2,"type":"Checkbox","parentId":7,"style":{"enabled":true,"varName":"bridgeday","text":"Brückentage auffüllen","preferredSize":[0,0],"alignment":"left","helpTip":null}},"item-3":{"id":3,"type":"Slider","parentId":6,"style":{"enabled":true,"varName":"UnschaerfefaktorSlider","preferredSize":[200,0],"alignment":"center","helpTip":null}},"item-4":{"id":4,"type":"StaticText","parentId":6,"style":{"enabled":true,"varName":"UnschaerfefaktorLabel","creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Unschaerfefaktor","justify":"left","preferredSize":[0,0],"alignment":"center","helpTip":null}},"item-5":{"id":5,"type":"Divider","parentId":0,"style":{"enabled":true,"varName":null}},"item-6":{"id":6,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":16,"alignChildren":["left","top"],"alignment":"center"}},"item-7":{"id":7,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-8":{"id":8,"type":"Divider","parentId":0,"style":{"enabled":true,"varName":null}},"item-9":{"id":9,"type":"Button","parentId":11,"style":{"enabled":true,"varName":"cancelBtn","text":"Abbrechen","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-10":{"id":10,"type":"Button","parentId":11,"style":{"enabled":true,"varName":"startBtn","text":"Start","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-11":{"id":11,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-12":{"id":12,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-13":{"id":13,"type":"StaticText","parentId":12,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Wieviele Wochen anhängen?","justify":"left","preferredSize":[0,0],"alignment":"center","helpTip":null}},"item-14":{"id":14,"type":"DropDownList","parentId":12,"style":{"enabled":true,"varName":"addWeeks","text":"DropDownList","listItems":"0,1,2,3,4,5","preferredSize":[73,0],"alignment":null,"selection":2,"helpTip":null}},"item-15":{"id":15,"type":"StaticText","parentId":6,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"0","justify":"left","preferredSize":[0,0],"alignment":"center","helpTip":null}}},"order":[0,12,13,14,6,4,3,15,5,7,1,2,8,11,9,10],"settings":{"importJSON":true,"indentSize":false,"cepExport":false,"includeCSSJS":true,"showDialog":true,"functionWrapper":false,"afterEffectsDockable":false,"itemReferenceList":"None"}}
+{"activeId":15,"items":{"item-0":{"id":0,"type":"Dialog","parentId":false,"style":{"enabled":true,"varName":null,"windowType":"Dialog","creationProps":{"su1PanelCoordinates":false,"maximizeButton":false,"minimizeButton":false,"independent":false,"closeButton":true,"borderless":false,"resizeable":false},"text":"Wochen auffuellen","preferredSize":[0,0],"margins":16,"orientation":"column","spacing":10,"alignChildren":["center","top"]}},"item-1":{"id":1,"type":"Checkbox","parentId":7,"style":{"enabled":true,"varName":"labweek","text":"Seminar- / Laborwoche beruecksichtigen","preferredSize":[0,0],"alignment":"left","helpTip":null}},"item-2":{"id":2,"type":"Checkbox","parentId":7,"style":{"enabled":true,"varName":"bridgeday","text":"Brueckentage auffuellen","preferredSize":[0,0],"alignment":"left","helpTip":null}},"item-3":{"id":3,"type":"Slider","parentId":6,"style":{"enabled":true,"varName":"UnschaerfefaktorSlider","preferredSize":[200,0],"alignment":"center","helpTip":null}},"item-4":{"id":4,"type":"StaticText","parentId":6,"style":{"enabled":true,"varName":"UnschaerfefaktorLabel","creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Unschaerfefaktor","justify":"left","preferredSize":[0,0],"alignment":"center","helpTip":null}},"item-5":{"id":5,"type":"Divider","parentId":0,"style":{"enabled":true,"varName":null}},"item-6":{"id":6,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":16,"alignChildren":["left","top"],"alignment":"center"}},"item-7":{"id":7,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-8":{"id":8,"type":"Divider","parentId":0,"style":{"enabled":true,"varName":null}},"item-9":{"id":9,"type":"Button","parentId":11,"style":{"enabled":true,"varName":"cancelBtn","text":"Abbrechen","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-10":{"id":10,"type":"Button","parentId":11,"style":{"enabled":true,"varName":"startBtn","text":"Start","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-11":{"id":11,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-12":{"id":12,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-13":{"id":13,"type":"StaticText","parentId":12,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Wieviele Wochen anhaengen?","justify":"left","preferredSize":[0,0],"alignment":"center","helpTip":null}},"item-14":{"id":14,"type":"DropDownList","parentId":12,"style":{"enabled":true,"varName":"addWeeks","text":"DropDownList","listItems":"0,1,2,3,4,5","preferredSize":[73,0],"alignment":null,"selection":2,"helpTip":null}},"item-15":{"id":15,"type":"StaticText","parentId":6,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"0","justify":"left","preferredSize":[30,0],"alignment":"center","helpTip":null}},"item-16":{"id":16,"type":"Divider","parentId":0,"style":{"enabled":true,"varName":null}},"item-17":{"id":17,"type":"Checkbox","parentId":0,"style":{"enabled":true,"varName":null,"text":"Anhaengen erzwingen","preferredSize":[0,0],"alignment":"left","helpTip":null}}},"order":[0,12,13,14,6,4,3,15,5,7,1,2,8,17,16,11,9,10],"settings":{"importJSON":true,"indentSize":false,"cepExport":false,"includeCSSJS":true,"showDialog":true,"functionWrapper":false,"afterEffectsDockable":false,"itemReferenceList":"None"}}
 */ 
 
 // DIALOG
@@ -176,8 +177,8 @@ var UnschaerfefaktorSlider = group2.add("slider", undefined, undefined, undefine
     UnschaerfefaktorSlider.value = 0; 
 
 var statictext2 = group2.add("statictext", undefined, undefined, {name: "statictext2"}); 
-    statictext2.text = "0";
-    statictext2.preferredSize.width = 15; 
+    statictext2.text = "0"; 
+    statictext2.preferredSize.width = 30; 
     statictext2.alignment = ["left","center"]; 
 
 // DIALOG
@@ -206,6 +207,13 @@ var bridgeday = group3.add("checkbox", undefined, undefined, {name: "bridgeday"}
 var divider2 = dialog.add("panel", undefined, undefined, {name: "divider2"}); 
     divider2.alignment = "fill"; 
 
+var forceAppend = dialog.add("checkbox", undefined, undefined, {name: "forceAppend"}); 
+    forceAppend.text = "Anhaengen erzwingen"; 
+    forceAppend.alignment = ["left","top"]; 
+
+var divider3 = dialog.add("panel", undefined, undefined, {name: "divider3"}); 
+    divider3.alignment = "fill"; 
+
 // GROUP4
 // ======
 var group4 = dialog.add("group", undefined, {name: "group4"}); 
@@ -219,6 +227,7 @@ var cancelBtn = group4.add("button", undefined, undefined, {name: "cancelBtn"});
 
 var startBtn = group4.add("button", undefined, undefined, {name: "startBtn"}); 
     startBtn.text = "Start"; 
+
 
 startBtn.onClick = function (){
     dialog.close();
@@ -307,18 +316,19 @@ for (var n = 0; n < items.length; n++) {
         $.writeln("DEBUG: Distances (all/even/odd) - ", dAll, " ", dEven, " ", dOdd);
 
         //Main Search and Add Logic
-        if (dAll <= maxDist) {
+        if (dAll <= maxDist || forceAppend.value == true) {
                 $.writeln("DEBUG: All Match for " + items[n]);               
                 
+                dayArray = labelPrev.split(",");       
+                $.writeln("DEBUG: Day array " + dayArray + " (len: "+dayArray.length+")");
+
                 //Add lab week if enabled and not already there
-                if(!labelPrev.split(",").includes(labWeek) && considerLabweek) {
+                if(!dayArray.includes(labWeek.toString()) && considerLabweek) {
                         items[n].label += ","+labWeek;
                         $.writeln("DEBUG: Add lab week " + labWeek); 
                 }
                 
-                //Add bridgedays if enabled and not already there
-                dayArray = labelPrev.split(",");       
-                $.writeln("DEBUG: Day array " + dayArray + " (len: "+dayArray.length+")");
+                //Add bridgedays if enabled and not already there                
                 $.writeln("DEBUG: Bridgeday array " + bridgeDaysForDay + " (len: "+bridgeDaysForDay.length+")");          
                 for (var i=0, len=bridgeDaysForDay.length; i < len ; i++) {
                         if(!dayArray.includes(bridgeDaysForDay[i]) && considerBridgedays && bridgeDaysForDay[i]) {
@@ -333,15 +343,16 @@ for (var n = 0; n < items.length; n++) {
         } else if (dOdd <= maxDist) {
                 $.writeln("DEBUG: Odd Match for " + items[n]);
 
+                dayArray = labelPrev.split(",");       
+                $.writeln("DEBUG: Day array " + dayArray + " (len: "+dayArray.length+")");
+
                 //Add lab week if enabled and not already there
-                if(!labelPrev.split(",").includes(labWeek) && considerLabweek && labWeek % 2 == 1) {
+                 if(!dayArray.includes(labWeek.toString()) && considerLabweek && labWeek % 2 == 1) {
                         items[n].label += ","+labWeek;
                         $.writeln("DEBUG: Add lab week " + labWeek); 
                 }
                 
-                //Add bridgedays if enabled and not already there
-                dayArray = labelPrev.split(",");       
-                $.writeln("DEBUG: Day array " + dayArray + " (len: "+dayArray.length+")");
+                //Add bridgedays if enabled and not already there               
                 $.writeln("DEBUG: Bridgeday array " + bridgeDaysForDay + " (len: "+bridgeDaysForDay.length+")");          
                 for (var i=0, len=bridgeDaysForDay.length; i < len ; i++) {
                         if(!dayArray.includes(bridgeDaysForDay[i]) && considerBridgedays && bridgeDaysForDay[i]  && bridgeDays[i] % 2 == 1) {
@@ -356,15 +367,16 @@ for (var n = 0; n < items.length; n++) {
         } else if (dEven <= maxDist) {
                 $.writeln("DEBUG: Even Match for " + items[n]);
 
+                dayArray = labelPrev.split(",");       
+                $.writeln("DEBUG: Day array " + dayArray + " (len: "+dayArray.length+")");
+
                 //Add lab week if enabled and not already there
-                if(!labelPrev.split(",").includes(labWeek) && considerLabweek && labWeek % 2 == 0) {
+                if(!dayArray.includes(labWeek.toString()) && considerLabweek && labWeek % 2 == 0) {
                         items[n].label += ","+labWeek;
                         $.writeln("DEBUG: Add lab week " + labWeek); 
                 }
                 
                 //Add bridgedays if enabled and not already there
-                dayArray = labelPrev.split(",");       
-                $.writeln("DEBUG: Day array " + dayArray + " (len: "+dayArray.length+")");
                 $.writeln("DEBUG: Bridgeday array " + bridgeDaysForDay + " (len: "+bridgeDaysForDay.length+")");          
                 for (var i=0, len=bridgeDaysForDay.length; i < len ; i++) {
                         if(!dayArray.includes(bridgeDaysForDay[i]) && considerBridgedays && bridgeDaysForDay[i]  && bridgeDays[i] % 2 == 0) {
